c570e193897384ac5b8ad8114c9a207cfab3fad6
Cristina Fierro <jazmin.brennan.duquesne@gmail.com>
2019-02-05 21:39:04 +0300

    Third Commit: modified factorial.cpp

--- lab1/factorial.cpp
+++ lab1/factorial.cpp
@@ -1,18 +1,21 @@
-#include<iostream> 
-using namespace std; 
-  
-unsigned int factorial(unsigned int n) 
-{ 
-    int res = 1, i; 
-    for (i = 2; i <= n; i++) 
-        res *= i; 
-    return res; 
-} 
-  
-int main() 
-{ 
+#include<iostream>
+using namespace std;
+
+unsigned int factorial(unsigned int n)
+{
+    int res = 1, i;
+    for (i = 2; i <= n; i++)
+        res *= i;
+    return res;
+}
+
+int main()
+{
 	int num;
+	int extra;
     cin >> num;
-    cout << factorial(num) << endl; 
-    return 0; 
-} 
+    cout << factorial(num) << endl;
+    cin >> extra;
+    cout << factorial(num) + extra << endl;
+    return 0;
+}
