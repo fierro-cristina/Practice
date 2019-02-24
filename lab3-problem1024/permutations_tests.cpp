4e98cf056051a311c3f313cb2eb35689b3a4964a
Cristina Fierro <jazmin.brennan.duquesne@gmail.com>
2019-02-24 17:34:42 +0300

    Update permutations_tests.cpp

--- lab3-problem1024/permutations_tests.cpp
+++ lab3-problem1024/permutations_tests.cpp
@@ -1,15 +1,86 @@
 #include "permutations.cpp"
 #include <gtest/gtest.h>
+#include <time.h>
+
 
 TEST(permutationTest, rightAns){
 	int P5[] = {4, 1, 5, 2, 3};
 	EXPECT_EQ(6, permutation(5, P5));
-	
+
 	int P7[] = { 4, 6, 7, 1, 5, 2, 3};
 	EXPECT_EQ(2, permutation(7, P7));
 	
 	int P9[] = { 6, 9, 8, 7, 4, 1, 5, 2, 3};
 	EXPECT_EQ(12, permutation(9, P9));
+
+}
+
+
+TEST(permutationElements, ZeroToThousand){
+	int P5[] = {4, 1, 5, 2, 3};
+	EXPECT_TRUE(sizeof(P5) <= 1000 && sizeof(P5) > 0);
+
+	int P7[] = { 4, 6, 7, 1, 5, 2, 3};
+	EXPECT_TRUE(sizeof(P7) <= 1000 && sizeof(P7) > 0);
+
+	int P9[] = { 6, 9, 8, 7, 4, 1, 5, 2, 3};
+	EXPECT_TRUE(sizeof(P9) <= 1000 && sizeof(P9) > 0);
+}
+
+TEST(permutationElements1, isArraySorted){
+	int P5[] = {4, 1, 5, 2, 3};
+	int P5_sorted[] = {1, 2, 3, 4, 5};
+	ASSERT_FALSE(P5 == P5_sorted);
+
+	int P7[] = {4, 6, 7, 1, 5, 2, 3};
+	int P7_sorted[] = {1, 2, 3, 4, 5, 6, 7};
+	ASSERT_FALSE(P7 == P7_sorted);
+
+	int P9[] = {6, 9, 8, 7, 4, 1, 5, 2, 3};
+	int P9_sorted[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+	ASSERT_FALSE(P9 == P9_sorted);
+}
+
+TEST(permuationRunTime, checkRuntimeP5){
+
+	clock_t start, end;
+	start = clock();
+	
+	int P5[] = {4, 1, 5, 2, 3};
+	permutation(5, P5);
+
+	end = clock();
+	double runtimeSEC = double(end-start)/CLOCKS_PER_SEC;
+
+	ASSERT_TRUE(runtimeSEC <= 0.003);
+}
+
+TEST(permuationRunTime1, checkRuntimeP7){
+
+	clock_t start, end;
+	start = clock();
+	
+	int P7[] = {4, 6, 7, 1, 5, 2, 3};
+	permutation(7, P7);
+
+	end = clock();
+	double runtimeSEC = double(end-start)/CLOCKS_PER_SEC;
+
+	ASSERT_TRUE(runtimeSEC <= 0.003);
+}
+
+TEST(permuationRunTime2, checkRuntimeP9){
+
+	clock_t start, end;
+	start = clock();
+	
+	int P9[] = {6, 9, 8, 7, 4, 1, 5, 2, 3};
+	permutation(9, P9);
+
+	end = clock();
+	double runtimeSEC = double(end-start)/CLOCKS_PER_SEC;
+
+	ASSERT_TRUE(runtimeSEC <= 0.003);
 }
 
 int main(int argc, char* argv[]) {
