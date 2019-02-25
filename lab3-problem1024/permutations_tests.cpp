16a5facd4ca3574ba662ba8f838a7b4a44692641
Cristina Fierro <jazmin.brennan.duquesne@gmail.com>
2019-02-25 13:41:33 +0300

    Update permutations_tests.cpp

--- lab3-problem1024/permutations_tests.cpp
+++ lab3-problem1024/permutations_tests.cpp
@@ -2,85 +2,67 @@
 #include <gtest/gtest.h>
 #include <time.h>
 
+int P5[] = {4, 1, 5, 2, 3};
+int P7[] = { 4, 6, 7, 1, 5, 2, 3};
+int P9[] = { 6, 9, 8, 7, 4, 1, 5, 2, 3};
 
-TEST(permutationTest, rightAns){
-	int P5[] = {4, 1, 5, 2, 3};
+TEST(permutation, rightAns){
 	EXPECT_EQ(6, permutation(5, P5));
-
-	int P7[] = { 4, 6, 7, 1, 5, 2, 3};
 	EXPECT_EQ(2, permutation(7, P7));
-	
-	int P9[] = { 6, 9, 8, 7, 4, 1, 5, 2, 3};
 	EXPECT_EQ(12, permutation(9, P9));
-
 }
 
 
-TEST(permutationElements, ZeroToThousand){
-	int P5[] = {4, 1, 5, 2, 3};
+TEST(permutation, ElementsZeroToThousand){
 	EXPECT_TRUE(sizeof(P5) <= 1000 && sizeof(P5) > 0);
-
-	int P7[] = { 4, 6, 7, 1, 5, 2, 3};
 	EXPECT_TRUE(sizeof(P7) <= 1000 && sizeof(P7) > 0);
-
-	int P9[] = { 6, 9, 8, 7, 4, 1, 5, 2, 3};
 	EXPECT_TRUE(sizeof(P9) <= 1000 && sizeof(P9) > 0);
 }
 
-TEST(permutationElements1, isArraySorted){
-	int P5[] = {4, 1, 5, 2, 3};
-	int P5_sorted[] = {1, 2, 3, 4, 5};
-	ASSERT_FALSE(P5 == P5_sorted);
+/*According to the mathematical rules, if the input permutation is sorted the order of its permutation will be equals to 1
+theta = sorted permuation; theta^1, where theta is elevated to the k order will give the right answer. 
+When the input permutation is not sorted, the order k will be bigger than 1*/
 
-	int P7[] = {4, 6, 7, 1, 5, 2, 3};
-	int P7_sorted[] = {1, 2, 3, 4, 5, 6, 7};
-	ASSERT_FALSE(P7 == P7_sorted);
-
-	int P9[] = {6, 9, 8, 7, 4, 1, 5, 2, 3};
-	int P9_sorted[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-	ASSERT_FALSE(P9 == P9_sorted);
+TEST(permutation, arrayNotSorted){
+	EXPECT_TRUE(permutation(5, P5) > 1);
+	EXPECT_TRUE(permutation(7, P7) > 1);
+	EXPECT_TRUE(permutation(9, P9) > 1);
 }
 
-TEST(permuationRunTime, checkRuntimeP5){
+TEST(permutation, checkRuntime){
 
-	clock_t start, end;
-	start = clock();
-	
-	int P5[] = {4, 1, 5, 2, 3};
+	clock_t startP5, startP7, startP9, endP5,endP7, endP9;
+	startP5 = clock();
 	permutation(5, P5);
-
-	end = clock();
-	double runtimeSEC = double(end-start)/CLOCKS_PER_SEC;
-
-	ASSERT_TRUE(runtimeSEC <= 0.003);
-}
-
-TEST(permuationRunTime1, checkRuntimeP7){
-
-	clock_t start, end;
-	start = clock();
+	endP5 = clock();
+	double runtimeSEC_P5 = double(endP5-startP5)/CLOCKS_PER_SEC;
 	
-	int P7[] = {4, 6, 7, 1, 5, 2, 3};
+	startP7 = clock();
 	permutation(7, P7);
+	endP7 = clock();
+	double runtimeSEC_P7 = double(endP7-startP7)/CLOCKS_PER_SEC;
 
-	end = clock();
-	double runtimeSEC = double(end-start)/CLOCKS_PER_SEC;
+	startP9 = clock();
+	permutation(9, P9);
+	endP9 = clock();
+	double runtimeSEC_P9 = double(endP9-startP9)/CLOCKS_PER_SEC;
 
-	ASSERT_TRUE(runtimeSEC <= 0.003);
+	EXPECT_TRUE(runtimeSEC_P5 <= 0.003 && runtimeSEC_P7 <= 0.003 && runtimeSEC_P9 <= 0.003);
 }
 
-TEST(permuationRunTime2, checkRuntimeP9){
+TEST(gcd, gcdNotZero){
+	EXPECT_TRUE(permutation(5, P5) > 0 
+		 && permutation(7, P7) > 0
+		 && permutation(9, P9) > 0);
+}
 
+TEST(gcd, gcdRuntime){
 	clock_t start, end;
 	start = clock();
-	
-	int P9[] = {6, 9, 8, 7, 4, 1, 5, 2, 3};
-	permutation(9, P9);
-
+	gcd(100, 85);
 	end = clock();
 	double runtimeSEC = double(end-start)/CLOCKS_PER_SEC;
-
-	ASSERT_TRUE(runtimeSEC <= 0.003);
+	EXPECT_TRUE(runtimeSEC <= 0.003);
 }
 
 int main(int argc, char* argv[]) {
